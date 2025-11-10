fede20f85495a81c1ac614f2cf0d0c097865383d
NikolG <greplova.nikol@gmail.com>
2025-11-10 14:28:22 +0100

    Slicing update

---
+++ lesson-1/main.py
@@ -0,0 +1,11 @@
+venecky = [1, 2, 4, 1, 6, 0, 1]
+
+# I want to display number 4 (third element)
+print(venecky[2])
+
+# I want to display 0 (second element from the end)
+print(venecky[-2])
+
+# Slicing -> first index : to but not through index 
+print(venecky[0:5])
+# If I want to start fom the first, or go all the way to last - I can 
