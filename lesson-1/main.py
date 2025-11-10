a5d632c7c092e64ff94c249bc28a9374c87e8bb4
Nikol (laptop 2) <greplova.nikol@gmail.com>
2025-11-10 18:11:14 +0100

    List methods update

--- lesson-1/main.py
+++ lesson-1/main.py
@@ -8,4 +8,26 @@ print(venecky[-2])
 
 # Slicing -> first index : to but not through index 
 print(venecky[0:5])
-# If I want to start fom the first, or go all the way to last - I can 
+# If I want to start fom the first, or go all the way to last - I can just leave that empty [:5] or [2:]
+
+# This is also a way to write last three elements, this is useful when I have a really large list:
+print(venecky[-3:])
+
+
+list_length = len(venecky)
+print(list_length)
+
+list_sum = sum(venecky)
+print(list_sum)
+
+list_min = min(venecky)
+print(list_min)
+
+list_max = max(venecky)
+print(list_max)
+
+list_sorted = sorted(venecky)
+print(list_sorted)
+
+list_sorted_backwards = sorted(venecky, reverse=True)
+print(list_sorted_backwards)
