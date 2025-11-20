c44bd259c85ff666428011512eac13c401647e25
Nikol (laptop 2) <greplova.nikol@gmail.com>
2025-11-20 17:52:08 +0100

    Second lesson progress

---
+++ lesson-2/modules.py
@@ -0,0 +1,11 @@
+# Tady importujeme knihovnu math:
+import math
+
+
+cislo = 3.45667333
+
+# Kdyz bych chtela zaokrouhlit nahoru:
+print(math.ceil(cislo))
+
+# Zaokrouhleni dolu:
+print(math.floor(cislo))
---
+++ lesson-2/stats.py
@@ -0,0 +1,8 @@
+# Here I am going to import it with an alias, so that I don't need to write out the whole name
+import statistics as stats
+
+
+randomnumber = 56
+cisla = [1, 2, 3, 4, 5, 6, randomnumber]
+
+print(stats.mean(cisla))
