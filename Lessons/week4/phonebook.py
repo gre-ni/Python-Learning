a403dfdae1ea2c461292e54c78df18957ba2226b
NikolG <greplova.nikol@gmail.com>
2026-02-03 23:06:53 +0100

    DictWriter csv

--- Lessons/week4/phonebook.py
+++ Lessons/week4/phonebook.py
@@ -5,5 +5,7 @@ number = input("Number: ")
 
 # Opens file, assigns to variable on the right + closes automatically after
 with open("phonebook.csv", "a") as file:
-    write = csv.writer(file) # Turn that file into a csv it can be written to
-    write.writerow([name, number])
+    write = csv.DictWriter(file, fieldnames=["name", "number"]) # Turn that file into a csv it can be written to
+    # compared to .writer, Dict will allow me to have a header row with keys
+    write.writerow({"name": name, "number": number})
+
