c4e8c164504ce9b9dd9359867ddcdc52408d1bbf
NikolG <greplova.nikol@gmail.com>
2026-03-24 15:03:39 +0100

    clock convert

---
+++ Regex/clock_convert.py
@@ -0,0 +1,46 @@
+import re
+import sys
+
+
+def main():
+    print(convert(input("Hours: ")))
+
+# 9:00 AM to 5:00 PM
+# 9 AM to 5 PM
+# 9:00 AM to 5 PM
+# 9 AM to 5:00 PM
+
+def convert(s):
+    if match := re.search(r"(?:(\d[0-2]?):?([0-5]\d)?) ?([AP]M) to (?:(\d[0-2]?):?([0-5]\d)?) ([AP]M)", s):
+
+        time_from_hours = match.group(1)
+        time_from_mins = match.group(2) or "00"
+        time_from_day = match.group(3)
+        
+        time_to_hours = match.group(4)
+        time_to_mins = match.group(5) or "00"
+        time_to_day = match.group(6)
+        
+        if time_from_day == "PM":
+            time_from_hours = int(time_from_hours) + 12
+        
+        if time_to_day == "PM":
+            time_to_hours = int(time_to_hours) + 12
+        
+        if time_from_day == "AM" and time_from_hours == "12":
+            time_from_hours = "00"
+        
+        if time_to_day == "AM" and time_to_hours == "12":
+            time_to_hours = "00" 
+        
+        return f"{time_from_hours}:{time_from_mins} to {time_to_hours}:{time_to_mins}"
+            
+    else:
+        raise ValueError
+
+
+...
+
+
+if __name__ == "__main__":
+    main()
---
+++ Regex/test_clock.py
@@ -0,0 +1,20 @@
+import pytest 
+from clock_convert import convert
+
+@pytest.mark.parametrize("time", ["9:00 AM to 5:00 PM", "9 AM to 5 PM", "9:00 AM to 5 PM", "9 AM to 5:00 PM"])
+def test_valid_formats(time):
+    assert convert(time) == "9:00 to 17:00"
+
+def test_alternative_time():
+    assert convert("1 PM to 11 PM") == "13:00 to 23:00"
+    
+def test_no_convert():
+    assert convert("7:00 AM to 9:00 AM") == "7:00 to 9:00"
+
+@pytest.mark.parametrize("wrong_format", ["5 to 6 PM", "12-14", "12 AM-2 PM"])
+def test_wrong_format(wrong_format):
+    with pytest.raises(ValueError):
+        convert(wrong_format)
+        
+def test_midnight():
+    assert convert("10 PM to 12:00 AM") == "22:00 to 00:00"
