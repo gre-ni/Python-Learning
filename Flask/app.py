386ef9a1e4262d9bc553357174a13210422ae333
NikolG <greplova.nikol@gmail.com>
2026-04-19 21:00:06 +0200

    refactor(flask): joining /greet route into /

--- Flask/app.py
+++ Flask/app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html") 
-
-@app.route("/greet", methods=["POST"])
-def greet():
-    return render_template("greet.html", name=request.form.get("name", "world"))
+    if request.method == "POST": 
+        # The only way to get there via post is to click the submit button
+        return render_template("greet.html", name=request.form.get("name", "world"))
+    else: 
+        # Get -> no form was submitted, so show form first
+        return render_template("index.html")
